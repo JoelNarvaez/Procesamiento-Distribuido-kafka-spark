ed2f30ba1988f1e0782a8d3299293f87ef7407e2
LorenzoAntonioLAPL <lorenzoopacheco06@gmail.com>
2026-06-07 18:43:38 -0600

    actualizacion

--- kafka/shared/kafka_client.js
+++ kafka/shared/kafka_client.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 const brokers = process.env.KAFKA_BROKERS
   ? process.env.KAFKA_BROKERS.split(",")
   : [
-      "192.168.10.101:9092",
-      "192.168.10.102:9092",
-      "192.168.10.103:9092"
+      "192.168.1.65:9092",
+      "192.168.1.66:9092",
+      "192.168.1.67:9092"
     ];
 
 function createKafkaClient(clientId) {
